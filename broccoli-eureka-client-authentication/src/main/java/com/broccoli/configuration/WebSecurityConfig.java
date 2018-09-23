1d80924628c2deefc7481c93c35d41a136a7258c
CrBatista <cristianbatista@outlook.es>
2018-09-23 16:42:47 +0100

    Don't want to expose AuthenticationManager, that's for Spring boot 1.5.x

--- broccoli-eureka-client-authentication/src/main/java/com/broccoli/configuration/WebSecurityConfig.java
+++ broccoli-eureka-client-authentication/src/main/java/com/broccoli/configuration/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.broccoli.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -17,6 +18,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
