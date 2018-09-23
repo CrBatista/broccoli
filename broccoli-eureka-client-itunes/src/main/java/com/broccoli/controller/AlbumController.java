db03823b1ad6c47b8c5f7f00047f7c9ebb674361
CrBatista <cristianbatista@outlook.es>
2018-09-23 14:33:25 +0100

    Added Security to iTunes MicroService endpoints

--- broccoli-eureka-client-itunes/src/main/java/com/broccoli/controller/AlbumController.java
+++ broccoli-eureka-client-itunes/src/main/java/com/broccoli/controller/AlbumController.java
@@ -3,6 +3,7 @@ package com.broccoli.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,6 +20,7 @@ public class AlbumController {
 
 	@GetMapping("/albums/{input}")
 	@ResponseBody
+	@PreAuthorize("hasAuthority('ADMIN')")
 	public List<Album> searchAlbums(@PathVariable("input") String input) {
 		return albumService.searchAlbums(input);
 	}
